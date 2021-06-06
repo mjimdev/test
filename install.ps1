e5d9fc13054bcd5c355d2e896159af74500b6436
mjimdev <85459544+mjimdev@users.noreply.github.com>
2021-06-06 15:40:32 -0500

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,48 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+############################################################
+ 
+# Author: Douglas Loyo, Sr. Solutions Architect @ MSDF
+ 
+# Description: Wrapper script that downloads the necesarry binaries and executes Ed-Fi installers.
+
+############################################################
+#Requires -Version 5
+#Requires -RunAsAdministrator
+# 1) Ensure the working directory exists
+$global:pathToWorkingDir = "C:\Ed-Fi\BinaryInstaller\"
+Write-Host "Step: Ensuring working path is accessible. ($global:pathToWorkingDir)"
+New-Item -ItemType Directory -Force -Path $pathToWorkingDir
+
+
+# 2) Download and unzip the github powershell scripts (in zip format)
+$packageUrl = "https://github.com/Nearshore-Devs/Ed-Fi-TPDM-Installer/archive/main.zip"
+$outputpath = "$global:pathToWorkingDir\main.zip"
+Invoke-WebRequest -Uri $packageUrl -OutFile $outputpath
+Expand-Archive -LiteralPath $outputpath -DestinationPath $global:pathToWorkingDir -Force
+
+# 3) Execute script
+#$global:pathToAssets = "$global:pathToWorkingDir\Ed-Fi-BinariesPowerShellInstaller-main\"
+#$pathToMainScript = "$global:pathToAssets\binaryInstall.ps1"
+#Invoke-Expression -Command $pathToMainScript
+
+
+Import-Module .\InstallModule -Force
+
+Write-HostInfo "Wrapper for the Ed-Fi binary installers."
+Write-Host "To install Ed-Fi run any of the following commands:" 
+Write-HostStep " Ed-Fi ODS/APi & Tools 5.2.0"
+Write-Host " Install-EdFi520Sandbox"
+Write-Host " Install-EdFi520SharedInstance"
+Write-Host " Install-EdFi520SandboxTPDM"
+Write-Host " Install-EdFi520SharedInstanceTPDM"
+Write-HostStep " Other Tools:"
+Write-Host "    Install-TPDMDescriptors 'apiURL' 'key' 'secret'"
+Write-Host "    Install-Chocolatey" 
+Write-Host "    Install-Chrome" 
+Write-Host "    Install-MsSSMS"
+Write-Host "    Install-NotepadPlusPlus"
+Write-Host ""
